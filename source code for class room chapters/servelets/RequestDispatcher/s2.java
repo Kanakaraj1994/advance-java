e3485b97ea8da92688e9ac4d389e856e62c8af5b
wa5 <47422872+wa5@users.noreply.github.com>
2021-02-01 07:56:23 +0530

    Create s2.java

---
+++ source code for class room chapters/servelets/RequestDispatcher/s2.java
@@ -0,0 +1,29 @@
+package com.dewana;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class s2
+ */
+@WebServlet("/s2")
+public class s2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		System.out.println("hi im second servelet");
+		out.println("hi im second servelt");
+	}
+
+	
+
+}
