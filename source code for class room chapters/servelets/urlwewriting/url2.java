66c31df69fe024f71620180d4a2258903a5907c4
wa5 <47422872+wa5@users.noreply.github.com>
2021-02-01 07:59:42 +0530

    Create url2.java

---
+++ source code for class room chapters/servelets/urlwewriting/url2.java
@@ -0,0 +1,32 @@
+package com.dewana;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servelet_url2
+ */
+@WebServlet("/servelet_url2")
+public class servelet_url2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String s=request.getParameter("user");
+	out.println("hi im first severlet"+s);
+	}
+
+}
