64f214e47792feff60f938a38ba653f316fe31c8
wa5 <47422872+wa5@users.noreply.github.com>
2021-02-01 08:05:47 +0530

    Create hiddenformfield2.java

---
+++ source code for class room chapters/servelets/hiddenformfields/hiddenformfield2.java
@@ -0,0 +1,33 @@
+package com.dewana;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class hiddenformfield2
+ */
+@WebServlet("/hiddenformfield2")
+public class hiddenformfield2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String s=request.getParameter("name");
+		System.out.println("hi im first severlet");
+		out.print("hi welcome  "+s);
+		
+	}
+
+}
