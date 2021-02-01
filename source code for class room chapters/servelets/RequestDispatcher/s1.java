a796d5ead8ebda358dcc3016a7c90b709073470b
wa5 <47422872+wa5@users.noreply.github.com>
2021-02-01 07:55:45 +0530

    Create s1.java

---
+++ source code for class room chapters/servelets/RequestDispatcher/s1.java
@@ -0,0 +1,31 @@
+package com.dewana;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/s1")
+public class s1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		System.out.println("hi im first severlet");
+		out.print("hi im first");
+		
+		RequestDispatcher rd= request.getRequestDispatcher("s2");
+		rd.forward(request, response);
+	}
+
+}
