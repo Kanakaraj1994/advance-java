549bc1e71a0dfecad6f855a1777aa567a9f8d39e
wa5 <47422872+wa5@users.noreply.github.com>
2021-02-03 08:19:46 +0530

    Create controller-servalet.java

---
+++ source code for class room chapters/jsp/mvc/controller-servalet.java
@@ -0,0 +1,36 @@
+package com.dewana;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class controller
+ */
+@WebServlet("/controller")
+public class controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		RequestDispatcher rd= request.getRequestDispatcher("mvcindex.jsp");
+		
+		String data=testmodle.getdata();
+		request.setAttribute("data", data);
+		rd.forward(request, response);
+		out.print("hi welcome  "+data);
+	}
+
+}
