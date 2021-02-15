cc1259e97d057241831510971b74618f1a6beda8
wa5 <47422872+wa5@users.noreply.github.com>
2021-02-15 07:52:12 +0530

    Create testcontroller.java

---
+++ source code for class room chapters/spring boot/testcontroller.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Testcontroller {
+
+	@RequestMapping("/test")
+	@ResponseBody
+	public String handletest() {
+		
+		return "hello this is it";
+	}
+	
+}
